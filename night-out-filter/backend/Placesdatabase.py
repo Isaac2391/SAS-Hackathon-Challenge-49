37039c2e6ccf4b6cb5fb5d532d88405892e42ddc
sxge12 <crawfordclarke123@gmail.com>
2025-10-25 17:50:59 +0100

    user database

--- night-out-filter/backend/Placesdatabase.py
+++ night-out-filter/backend/Placesdatabase.py
@@ -7,27 +7,23 @@ def initialisedb():
 
     c = conn.cursor()
     c.execute("""CREATE TABLE IF NOT EXISTS places(
-        place_ID TEXT PRIMARY KEY NOT NULL,
-        Name TEXT,
-        Vicinty TEXT,
-        Types TEXT,
-        Rating REAL,
-        User_rating_total INTEGER,
-        business_status TEXT,
-        lat REAL,
-        lng REAL
+        User_ID TEXT PRIMARY KEY,
+        Name Text
     )""")
 
     conn.commit()
     conn.close()
     
-def searchandstore(storecat):
-    Search(storecat)
+def UserStorage(Name):
     conn = sq.connect("Places.db")
     
-    c = conn.cursor
+    c = conn.cursor()
     
-    c.execute("INSERT INTO expenses")
+    c.execute("INSERT INTO Places (Name) VALUEs (?)",((Name,)))
+    
+    conn.commit()
+    
+    conn.close()
     
     
     
